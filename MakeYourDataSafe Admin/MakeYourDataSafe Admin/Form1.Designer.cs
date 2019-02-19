8c02f5d58a0eb2b6481393e76a258502f1a7759b
Pogácsa Sajtos <gernyimark@gmail.com>
2019-02-19 18:26:45 +0100

    Admin design

--- MakeYourDataSafe Admin/MakeYourDataSafe Admin/Form1.Designer.cs
+++ MakeYourDataSafe Admin/MakeYourDataSafe Admin/Form1.Designer.cs
@@ -28,13 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ip = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Computers = new System.Windows.Forms.DataGridView();
+            this.keys = new System.Windows.Forms.Button();
+            this.msg = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Computers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.HeaderText = "PC Name";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            this.name.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.name.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // ip
+            // 
+            this.ip.HeaderText = "IP Address";
+            this.ip.Name = "ip";
+            this.ip.ReadOnly = true;
+            this.ip.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.ip.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.id.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Computers
+            // 
+            this.Computers.AllowUserToAddRows = false;
+            this.Computers.AllowUserToDeleteRows = false;
+            this.Computers.AllowUserToResizeColumns = false;
+            this.Computers.AllowUserToResizeRows = false;
+            this.Computers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Computers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.ip,
+            this.name});
+            this.Computers.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Computers.Location = new System.Drawing.Point(0, 6);
+            this.Computers.MultiSelect = false;
+            this.Computers.Name = "Computers";
+            this.Computers.ReadOnly = true;
+            this.Computers.RowHeadersVisible = false;
+            this.Computers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Computers.Size = new System.Drawing.Size(303, 321);
+            this.Computers.StandardTab = true;
+            this.Computers.TabIndex = 1;
+            this.Computers.SelectionChanged += new System.EventHandler(this.rowChange);
+            // 
+            // keys
+            // 
+            this.keys.Location = new System.Drawing.Point(309, 6);
+            this.keys.Name = "keys";
+            this.keys.Size = new System.Drawing.Size(121, 36);
+            this.keys.TabIndex = 2;
+            this.keys.Text = "Keystrokes";
+            this.keys.UseVisualStyleBackColor = true;
+            this.keys.Click += new System.EventHandler(this.keys_Click);
+            // 
+            // msg
+            // 
+            this.msg.Location = new System.Drawing.Point(309, 48);
+            this.msg.Name = "msg";
+            this.msg.Size = new System.Drawing.Size(121, 36);
+            this.msg.TabIndex = 3;
+            this.msg.Text = "Send Message";
+            this.msg.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(437, 333);
+            this.Controls.Add(this.msg);
+            this.Controls.Add(this.keys);
+            this.Controls.Add(this.Computers);
+            this.Name = "Form1";
+            this.Text = "MakeYourDataSafe Admin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Computers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void Computers_SelectionChanged(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ip;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridView Computers;
+        private System.Windows.Forms.Button keys;
+        private System.Windows.Forms.Button msg;
     }
 }
 
---
+++ MakeYourDataSafe Admin/MakeYourDataSafe Admin/Functions.cs
@@ -0,0 +1,63 @@
+﻿using Firebase.Login;
+using Firebase.Database;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using Firebase.Database.Query;
+using System.Reactive.Linq;
+
+namespace MakeYourDataSafe_Admin
+{
+    class Functions
+    {
+        private const string database = "https://myds-5f6f8.firebaseio.com/";
+        private FirebaseClient client = new FirebaseClient(database);
+        private async Task GetKeystrokes()
+        {
+            try
+            {
+                var client = new FirebaseClient(database);
+                var child = client.Child("Keystrokes");
+                var users = await child.OnceAsync<Login>();
+                foreach (var i in users)
+                {
+                    if (i.Object.username == log_user.Text && i.Object.password == Hash(log_pass.Text))
+                    {
+                        if (i.Key != "")
+                        {
+                            username = i.Object.username;
+                            await child.Child(i.Key).PutAsync(new Login() { username = i.Object.username, password = i.Object.password, Timestamp = i.Object.Timestamp, online = true });
+                            Chat ablak = new Chat();
+                            ablak.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            throw new DataException("Helytelen felhasználónév vagy jelszó.");
+                        }
+                        break;
+                    }
+                }
+            }
+            catch (DataException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (FirebaseException ex)
+            {
+                MessageBox.Show("Hiba az adatbázissal.\r\n" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nem várt hiba történt.\r\n" + ex.Message);
+            }
+        }
+    }
+}
